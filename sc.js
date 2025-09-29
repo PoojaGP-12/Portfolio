96c36604b120c5e805a32bfac602405654782471
Pooja G P <119751492+PoojaGP-12@users.noreply.github.com>
2025-09-29 19:14:49 +0530

    Add files via upload

--- sc.js
+++ sc.js
@@ -1,11 +1,8 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        if (section) {
-            section.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
